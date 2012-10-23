6be8c66e18b0287497c35a4de2a5d4915f885f63
Cosmic Cat <cosmic.cat144@gmail.com>
2012-10-23 09:23:12 -0700

    debugging changes

--- e17.rb
+++ e17.rb
@@ -4,66 +4,44 @@
 
 #NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
 
-lettersCount = 0
-for i in 1..999 do
-  threeDigits = sprintf("%03d", i)
-  # hundreds
-  lettersCount += case threeDigits[0]
-    when '0' then 0
-    when '1' then 'onehundredand'.length
-    when '2' then 'twohundredand'.length
-    when '3' then 'threehundredand'.length
-    when '4' then 'fourhundredand'.length
-    when '5' then 'fivehundredand'.length
-    when '6' then 'sixhundredand'.length
-    when '7' then 'sevenhundredand'.length
-    when '8' then 'eighthundredand'.length
-    when '9' then 'ninehundredand'.length
-  end
-
-  theFuckingTeens = false
-  lettersCount += case threeDigits[1]
-    when '0' then 0
-    when '1' then theFuckingTeens = true ; 0
-    when '2' then 'twenty'.length
-    when '3' then 'thirty'.length
-    when '4' then 'fourty'.length
-    when '5' then 'fifty'.length
-    when '6' then 'sixty'.length
-    when '7' then 'seventy'.length
-    when '8' then 'eighty'.length
-    when '9' then 'ninety'.length
+lettersCount = ''
+    when '4' then 'fourty'
+    when '5' then 'fifty'
+    when '6' then 'sixty'
+    when '7' then 'seventy'
+    when '8' then 'eighty'
+    when '9' then 'ninety'
   end
 
   if theFuckingTeens
     lettersCount += case threeDigits[2]
-      when '0' then 'ten'.length
-      when '1' then 'eleven'.length
-      when '2' then 'twelve'.length
-      when '3' then 'thirteen'.length
-      when '4' then 'fourteen'.length
-      when '5' then 'fifteen'.length
-      when '6' then 'sixteen'.length
-      when '7' then 'seventeen'.length
-      when '8' then 'eighteen'.length
-      when '9' then 'nineteen'.length
+      when '0' then 'ten'
+      when '1' then 'eleven'
+      when '2' then 'twelve'
+      when '3' then 'thirteen'
+      when '4' then 'fourteen'
+      when '5' then 'fifteen'
+      when '6' then 'sixteen'
+      when '7' then 'seventeen'
+      when '8' then 'eighteen'
+      when '9' then 'nineteen'
     end
 
   else
     lettersCount += case threeDigits[1]
-      when '0' then 0
-      when '1' then 'one'.length
-      when '2' then 'two'.length
-      when '3' then 'three'.length
-      when '4' then 'four'.length
-      when '5' then 'five'.length
-      when '6' then 'six'.length
-      when '7' then 'seven'.length
-      when '8' then 'eight'.length
-      when '9' then 'nine'.length
+      when '0' then ''
+      when '1' then 'one'
+      when '2' then 'two'
+      when '3' then 'three'
+      when '4' then 'four'
+      when '5' then 'five'
+      when '6' then 'six'
+      when '7' then 'seven'
+      when '8' then 'eight'
+      when '9' then 'nine'
     end
 
   end
 end
 
-puts lettersCount + "onethousand".length
+puts lettersCount + "onethousand"
